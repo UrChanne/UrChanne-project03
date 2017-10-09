3483ec9af07a7be40ba2c5414c86b3f325325038
bitam <mazloombita@foothill.edu>
2017-10-09 09:59:35 -0700

    Initial course material

---
+++ src/game/GuessOrSink.java
@@ -0,0 +1,57 @@
+package game;
+
+// imports the Scanner class to use for reading from System.in
+import java.util.Scanner;
+
+/**
+ * A game in which the goal is for the user to guess a word
+ * to save a ship from sinking. 
+ * 
+ * @author Foothill College, [YOUR NAME HERE]
+ */
+public class GuessOrSink 
+{
+	/**
+	 * Stores a word for the user to guess. Then, prompts the user to enter 
+	 * a single letter for their guess. Draws an ASCII representation of a 
+	 * ship depending on whether the user guessed correctly.
+	 * @param args    not used
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO: Select a 4 letter word to guess.	
+		
+		
+		// TODO: Keep track of the counters such as:
+		//      the current number of total guesses from user,
+		//      the number of allowable guesses,
+		//      etc.
+
+		
+		// TODO: Create an object of type Scanner to read user input.
+
+		
+		// Let's ROCK! Starting with introduction to the game.
+		// TODO: Provide a welcome message.
+
+
+		// TODO: Prompt user for the first guess.
+		// TODO: Use the Scanner object to read the next word.
+		// TODO: Store the first character and check whether it is a correct guess.
+        // TODO: When the guess is correct, let the user know.
+		//       Otherwise, redraw the ship sinking.	
+
+
+		// TODO: Prompt the user for the second guess.
+		//       Repeat.
+
+		
+		// TODO: Continue until either:
+		//       - the number of guesses exceeds the allowable number,
+		//         which should result in a sunken ship.
+		//       - the user has won,
+		//         at which point draw the ship in its fully operating mode!
+
+		
+	} 
+}
